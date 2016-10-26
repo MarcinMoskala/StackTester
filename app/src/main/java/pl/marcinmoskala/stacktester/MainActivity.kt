9c5924f6de2c23afa81b8e96d2b3ebc9516f6897
MarcinMoskala <marcinmoskala@gmail.com>
2016-10-26 19:47:39 +0200

    Make SELECT option

--- app/src/main/java/pl/marcinmoskala/stacktester/MainActivity.kt
+++ app/src/main/java/pl/marcinmoskala/stacktester/MainActivity.kt
@@ -14,36 +14,15 @@ import kotlin.properties.Delegates
 
 abstract class MainActivity : AppCompatActivity() {
 
-    val flagOptions = mapOf<String, Int>(
-            "FLAG_ACTIVITY_NEW_TASK" to FLAG_ACTIVITY_NEW_TASK,
-            "FLAG_ACTIVITY_CLEAR_TOP" to FLAG_ACTIVITY_CLEAR_TOP,
-            "FLAG_ACTIVITY_SINGLE_TOP" to FLAG_ACTIVITY_SINGLE_TOP,
-            "FLAG_ACTIVITY_NO_HISTORY" to FLAG_ACTIVITY_NO_HISTORY,
-            "FLAG_ACTIVITY_MULTIPLE_TASK" to FLAG_ACTIVITY_MULTIPLE_TASK,
-            "FLAG_ACTIVITY_FORWARD_RESULT" to FLAG_ACTIVITY_FORWARD_RESULT,
-            "FLAG_ACTIVITY_PREVIOUS_IS_TOP" to FLAG_ACTIVITY_PREVIOUS_IS_TOP,
-            "FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS" to FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS,
-            "FLAG_ACTIVITY_BROUGHT_TO_FRONT" to FLAG_ACTIVITY_BROUGHT_TO_FRONT,
-            "FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY" to FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY,
-            "FLAG_ACTIVITY_NEW_DOCUMENT" to FLAG_ACTIVITY_NEW_DOCUMENT,
-            "FLAG_ACTIVITY_NO_USER_ACTION" to FLAG_ACTIVITY_NO_USER_ACTION,
-            "FLAG_ACTIVITY_REORDER_TO_FRONT" to FLAG_ACTIVITY_REORDER_TO_FRONT,
-            "FLAG_ACTIVITY_NO_ANIMATION" to FLAG_ACTIVITY_NO_ANIMATION,
-            "FLAG_ACTIVITY_CLEAR_TASK" to FLAG_ACTIVITY_CLEAR_TASK,
-            "FLAG_ACTIVITY_TASK_ON_HOME" to FLAG_ACTIVITY_TASK_ON_HOME,
-            "FLAG_ACTIVITY_RETAIN_IN_RECENTS" to FLAG_ACTIVITY_RETAIN_IN_RECENTS,
-            "FLAG_ACTIVITY_LAUNCH_ADJACENT" to FLAG_ACTIVITY_LAUNCH_ADJACENT)
-
     val name = this.javaClass.simpleName + " num " + counter++
-
     var chosenFlags: List<Int> = emptyList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        buttonA.onClickShowActivityWithChosenFlag<A>()
-        buttonB.onClickShowActivityWithChosenFlag<B>()
-        buttonC.onClickShowActivityWithChosenFlag<C>()
+        buttonA.onClickStartActivityWithChosenFlag<A>()
+        buttonB.onClickStartActivityWithChosenFlag<B>()
+        buttonC.onClickStartActivityWithChosenFlag<C>()
         chooseFlagSpinner.setUp()
         title = name
         activityStack += this
@@ -61,29 +40,58 @@ abstract class MainActivity : AppCompatActivity() {
     private fun Spinner.setUp() {
         adapter = ArrayAdapter(context, android.R.layout.simple_spinner_dropdown_item, flagOptions.keys.toMutableList())
         onItemSelectedListener {
-            onItemSelected { adapterView, view, i, l -> addChosenFlag(flagOptions.toList()[i]) }
+            onItemSelected { adapterView, view, i, l -> onItemChosen(i) }
         }
     }
 
-    fun addChosenFlag(flag: Pair<String, Int>) {
+    private fun onItemChosen(position: Int) {
+        val flag = flagOptions.toList()[position]
+        if (flag.second != 0) {
+            addFlag(flag)
+        }
+    }
+
+    private fun addFlag(flag: Pair<String, Int>) {
         chosenFlags += flag.second
         flagsMonitor.text = flagsMonitor.text.toString() + "\n" + flag.first
     }
 
-    private inline fun <reified T : Activity> View.onClickShowActivityWithChosenFlag() {
+    private inline fun <reified T : Activity> View.onClickStartActivityWithChosenFlag() {
         setOnClickListener { startActivityWithChosenFlag<T>() }
     }
 
     private inline fun <reified T : Activity> startActivityWithChosenFlag() {
         val intent = intentFor<T>()
-        for(f in chosenFlags) intent.addFlags(f)
+        for (f in chosenFlags) intent.addFlags(f)
         startActivity(intent)
     }
-}
 
-var counter = 0
-var activityStack: List<MainActivity> by Delegates.observable(listOf()) { p, o, n ->
-    n.forEach { it.updateStackMonitor() }
+    companion object {
+        val flagOptions = mapOf<String, Int>(
+                "SELECT TO ADD" to 0,
+                "FLAG_ACTIVITY_NEW_TASK" to FLAG_ACTIVITY_NEW_TASK,
+                "FLAG_ACTIVITY_CLEAR_TOP" to FLAG_ACTIVITY_CLEAR_TOP,
+                "FLAG_ACTIVITY_SINGLE_TOP" to FLAG_ACTIVITY_SINGLE_TOP,
+                "FLAG_ACTIVITY_NO_HISTORY" to FLAG_ACTIVITY_NO_HISTORY,
+                "FLAG_ACTIVITY_MULTIPLE_TASK" to FLAG_ACTIVITY_MULTIPLE_TASK,
+                "FLAG_ACTIVITY_FORWARD_RESULT" to FLAG_ACTIVITY_FORWARD_RESULT,
+                "FLAG_ACTIVITY_PREVIOUS_IS_TOP" to FLAG_ACTIVITY_PREVIOUS_IS_TOP,
+                "FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS" to FLAG_ACTIVITY_EXCLUDE_FROM_RECENTS,
+                "FLAG_ACTIVITY_BROUGHT_TO_FRONT" to FLAG_ACTIVITY_BROUGHT_TO_FRONT,
+                "FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY" to FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY,
+                "FLAG_ACTIVITY_NEW_DOCUMENT" to FLAG_ACTIVITY_NEW_DOCUMENT,
+                "FLAG_ACTIVITY_NO_USER_ACTION" to FLAG_ACTIVITY_NO_USER_ACTION,
+                "FLAG_ACTIVITY_REORDER_TO_FRONT" to FLAG_ACTIVITY_REORDER_TO_FRONT,
+                "FLAG_ACTIVITY_NO_ANIMATION" to FLAG_ACTIVITY_NO_ANIMATION,
+                "FLAG_ACTIVITY_CLEAR_TASK" to FLAG_ACTIVITY_CLEAR_TASK,
+                "FLAG_ACTIVITY_TASK_ON_HOME" to FLAG_ACTIVITY_TASK_ON_HOME,
+                "FLAG_ACTIVITY_RETAIN_IN_RECENTS" to FLAG_ACTIVITY_RETAIN_IN_RECENTS,
+                "FLAG_ACTIVITY_LAUNCH_ADJACENT" to FLAG_ACTIVITY_LAUNCH_ADJACENT)
+        var counter = 0
+        var activityStack: List<MainActivity> by Delegates.observable(listOf()) { p, o, n ->
+            n.forEach { it.updateStackMonitor() }
+        }
+    }
 }
 
 class A : MainActivity()
